f835838ecfc2d8aa7e4dc48ee5030f66b3141923
Yamen Shahin <yamenshahin@gmail.com>
2021-10-19 12:52:28 +0200

    Format 1D and 5D

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <div class="col-sm"><FChart /></div>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm"></div>
+        <div class="col-sm"><FChart /></div>
         <div class="col-sm"><FCompanyProfile /></div>
         <div class="col-sm">One of three columns</div>
       </div>
